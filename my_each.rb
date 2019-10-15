40fcf1ce9c7407d7e4ea1de57e21ea5b59a23a9e
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-15 15:28:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
 def my_each(collection)
   counter = 0
-  while collection.length >= 0
+  while counter < collection.length
     yield collection[counter]
     counter = counter + 1
   end
   collection
-  end
 end
