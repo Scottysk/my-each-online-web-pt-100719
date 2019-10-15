75a35f54b75c1d6e486ddf922723ff8c1dd1b9da
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-15 15:26:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,9 @@
 def my_each(collection)
-  if block_given?
   counter = 0
   while collection.length >= 0
     yield collection[counter]
     i = i + 1
   end
   collection
-  else 
-    puts ""
   end
 end
